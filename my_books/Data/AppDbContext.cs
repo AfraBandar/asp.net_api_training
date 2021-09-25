0f62389c87d56a60c414194916b3ab4badce2ecf
ash <fitoash666@gmail.com>
2021-09-25 13:02:47 +0300

    asp.net we bapi training

---
+++ my_books/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;//important to add so we can use DbContext base class
+using my_books.Data.Models;// used by Book class parameter 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data
+{
+    public class AppDbContext : DbContext // bridge file btween c# models and sql db tables
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            public DbSet<Book> Books { get; set; } //<book> is the c# model and variabl Book is the name of the tabel
+        }
+    }
+}
