f4d989e3151669d7483fc96febc84bb820fc3d22
ash <fitoash666@gmail.com>
2021-09-28 23:13:00 +0300

    added all crud operations

--- my_books/Controllers/BooksController.cs
+++ my_books/Controllers/BooksController.cs
@@ -19,10 +19,39 @@ namespace my_books.Controllers
             _booksService = booksService;
         }
 
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _booksService.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("get-book-id/{id}")]//the {id} must match the name of the parameter
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
+        }
+
         [HttpPost("add-book")]
         public IActionResult AddBook ([FromBody]BookMV book)
         {
             _booksService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById (int id , [FromBody] BookMV book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+             _booksService.DeleteBookById(id);
             return Ok();
         }
     }
--- my_books/Data/Services/BooksService.cs
+++ my_books/Data/Services/BooksService.cs
@@ -33,5 +33,46 @@ namespace my_books.Data.Services
             _context.Books.Add(_book);
             _context.SaveChanges();
         }
+
+        //returns ALL books from the table
+        public List<Book> GetAllBooks()
+        {
+            var allBooks = _context.Books.ToList();
+            return allBooks;
+        }//or write public List<Book> GetAllBooks() => _context.Books.ToList();
+
+        //returns a single book
+        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault( n => n.Id == bookId);
+
+        public Book UpdateBookById(int bookId, BookMV book)// bookMV type to update only the specific proprties in the bookMV
+        {
+            //check if book exist
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
+            if (_book!=null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.IsRead ? book.DateRead.Value : null;
+                _book.Rate = book.IsRead ? book.Rate.Value : null;
+                _book.Genre = book.Genre;
+                _book.Author = book.Author;
+                _book.CoverUrl = book.CoverUrl;
+
+                _context.SaveChanges();
+    
+            }
+            return _book;
+        }
+
+        public void DeleteBookById (int bookId)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
+            if (_book != null)
+            {
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
+        }
     }
 }
